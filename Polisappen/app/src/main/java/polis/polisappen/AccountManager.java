3ae6f30feab9589133fece2be7cebd7273944345
Robin Ellgren <robel708@student.liu.se>
2018-02-06 16:21:48 +0100

    refactored code

--- Polisappen/app/src/main/java/polis/polisappen/AccountManager.java
+++ Polisappen/app/src/main/java/polis/polisappen/AccountManager.java
@@ -1,11 +1,52 @@
 package polis.polisappen;
 
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.nfc.NfcAdapter;
+import android.nfc.NfcManager;
+import android.widget.Toast;
+
 public class AccountManager {
+    private NfcAdapter adapter;
+    private Context context;
+    private PendingIntent pendingIntent;
+
+    public AccountManager(){
+
+    }
+
+    public AccountManager(NfcManager manager, Context context){
+        adapter = manager.getDefaultAdapter();
+        this.context = context;
+        if(adapter == null){
+            Toast.makeText(context, "Den här enheten har inte NFC", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(!adapter.isEnabled()){
+            Toast.makeText(context, "Du måste aktivera NFC", Toast.LENGTH_SHORT).show();
+            return;
+        }
+    }
 
 
     /* should return the auth-token, if it has not expired, then it should get a new one.*/
     public void getAuthToken(){
+        pendingIntent = PendingIntent.getActivity(context, 0,
+                new Intent(context, context.getClass())
+                        .addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+    }
 
+    protected void onResume() {
+        adapter.enableForegroundDispatch(R.id.activity_main, pendingIntent, null, null);
+    }
+
+    protected void onNewIntent(Intent intent) {
+        //setIntent(intent);
+        resolveIntent(intent);
+    }
+    private void resolveIntent(Intent intent) {
+        Toast.makeText(context, "WHATERVER", Toast.LENGTH_SHORT).show();
     }
 
     public boolean isLoggedIn(){
--- Polisappen/app/src/main/java/polis/polisappen/MainActivity.java
+++ Polisappen/app/src/main/java/polis/polisappen/MainActivity.java
@@ -9,46 +9,30 @@ import android.os.Bundle;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private NfcAdapter adapter;
     private PendingIntent pendingIntent;
+    public static AccountManager AUTHORIZATION;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final AccountManager Auth = new AccountManager();
         NfcManager NFCManager = (NfcManager)this.getSystemService(NFC_SERVICE);
-        adapter = NFCManager.getDefaultAdapter();
-        if(adapter == null){
-            Toast.makeText(this, "Den här enheten har inte NFC", Toast.LENGTH_SHORT).show();
-            finish();
-            return;
-        }
-        if(!adapter.isEnabled()){
-            Toast.makeText(this, "Du måste aktivera NFC", Toast.LENGTH_SHORT).show();
-            finish();
-            return;
-        }
-        if(!Auth.isLoggedIn()){
+        AUTHORIZATION = new AccountManager(NFCManager,this);
+
+        if(!AUTHORIZATION.isLoggedIn()){
            setContentView(R.layout.login_screen);
         }
-        pendingIntent = PendingIntent.getActivity(this, 0,
-                new Intent(this, this.getClass())
-                        .addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        adapter.enableForegroundDispatch(this, pendingIntent, null, null);
+        AUTHORIZATION.onResume();
     }
 
     @Override
     protected void onNewIntent(Intent intent) {
-        setIntent(intent);
-        resolveIntent(intent);
-    }
-    private void resolveIntent(Intent intent) {
-        Toast.makeText(this, "WHATERVER", Toast.LENGTH_SHORT).show();
+        AUTHORIZATION.onNewIntent(intent);
     }
 
     //Daniel was here
