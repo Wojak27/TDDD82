4a1682f233eba76f66390c541e44a49e99518990
Robin Ellgren <robel708@student.liu.se>
2018-02-06 14:26:05 +0100

    lite mer nfc

--- Polisappen/app/src/main/java/polis/polisappen/MainActivity.java
+++ Polisappen/app/src/main/java/polis/polisappen/MainActivity.java
@@ -1,23 +1,39 @@
 package polis.polisappen;
 
+import android.content.Intent;
 import android.nfc.NfcAdapter;
 import android.nfc.NfcManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
         final AccountManager Auth = new AccountManager();
         NfcManager NFCManager = (NfcManager)this.getSystemService(NFC_SERVICE);
         NfcAdapter NFCAdapter = NFCManager.getDefaultAdapter();
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        if(NFCAdapter == null){
+            Toast.makeText(this, "Den här enheten har inte NFC", Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+        if(!NFCAdapter.isEnabled()){
+            Toast.makeText(this, "Du måste aktivera NFC", Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
         if(!Auth.isLoggedIn()){
            setContentView(R.layout.login_screen);
         }
+        handleNFC(getIntent());
+    }
 
+    private void handleNFC(Intent intent){
+        
     }
 
     //Daniel was here
