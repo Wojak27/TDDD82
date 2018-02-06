3bf7b4c5c9272297ef33978af256697cdd858845
Wojaczek27 <wojaczek27@gmail.com>
2018-02-06 14:37:48 +0100

    massa fel

---
+++ Polisappen/app/src/main/java/polis/polisappen/LocalDatabase/User.java
@@ -0,0 +1,24 @@
+package polis.polisappen.LocalDatabase;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+/**
+ * Created by karolwojtulewicz on 2018-02-06.
+ */
+
+@Entity
+public class User {
+    @PrimaryKey
+    private int uid;
+
+    @ColumnInfo(name = "first_name")
+    private String firstName;
+
+    @ColumnInfo(name = "last_name")
+    private String lastName;
+
+    // Getters and setters are ignored for brevity,
+    // but they're required for Room to work.
+}
