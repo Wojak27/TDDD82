793633f76a9c3bbd0be843fa63086d703721caa7
Wojaczek27 <wojaczek27@gmail.com>
2018-02-06 14:41:27 +0100

    First instance of local database

--- Polisappen/app/src/main/java/polis/polisappen/LocalDatabase/User.java
+++ Polisappen/app/src/main/java/polis/polisappen/LocalDatabase/User.java
@@ -1,6 +1,8 @@
 package polis.polisappen.LocalDatabase;
 
-import android.arch.persistence.room.Database;
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
