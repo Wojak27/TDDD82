e62be9ee0256b5e24b408a68876bf11c754f8b51
Robin Ellgren <robel708@student.liu.se>
2018-02-06 13:43:29 +0100

    fixade dannes

---
+++ Polisappen/app/src/main/java/polis/polisappen/AccountManager.java
@@ -0,0 +1,13 @@
+package polis.polisappen;
+
+public class AccountManager {
+
+
+    /* should return the auth-token, if it has not expired, then it should get a new one.*/
+    public void getAuthToken(){
+
+    }
+
+
+
+}
