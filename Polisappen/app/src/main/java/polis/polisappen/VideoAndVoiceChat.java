36a266bfc68ef1fd2bc0f532e7ea7c1f770ff63d
Linnea <linlu607@student.liu.se>
2018-02-07 13:13:10 +0100

    Sinch 1

--- Polisappen/app/src/main/java/polis/polisappen/VideoAndVoiceChat.java
+++ Polisappen/app/src/main/java/polis/polisappen/VideoAndVoiceChat.java
@@ -1,9 +1,25 @@
 package polis.polisappen;
 
+import android.app.Activity;
+import android.content.Context;
+
+import com.sinch.android.rtc.Sinch;
+import com.sinch.android.rtc.SinchClient;
 /**
  * Created by linnea on 2018-02-07.
  */
 
-public class VideoAndVoiceChat {
+public class VideoAndVoiceChat extends Activity {
 
+    public void initSinch ()
+    {
+    // Instantiate a SinchClient using the SinchClientBuilder.
+    Context context = this.getApplicationContext();
+    SinchClient sinchClient = Sinch.getSinchClientBuilder().context(context)
+            .applicationKey("955e2079-38f0-43d5-af9e-80e4f3ade26d")
+            .applicationSecret("TZOvC9lH6k2wmJzWHEXh2Q==")
+            .environmentHost("sandbox.sinch.com")
+            .userId("<user id>")
+            .build();
+    }
 }
