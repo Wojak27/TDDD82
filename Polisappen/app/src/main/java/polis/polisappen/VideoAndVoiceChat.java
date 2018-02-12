ebdcd49b341ce67ef09d816db6dc3117d12940d4
Linnea <linlu607@student.liu.se>
2018-02-12 15:49:03 +0100

    ...

--- Polisappen/app/src/main/java/polis/polisappen/VideoAndVoiceChat.java
+++ Polisappen/app/src/main/java/polis/polisappen/VideoAndVoiceChat.java
@@ -6,15 +6,20 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.sinch.android.rtc.ClientRegistration;
+import com.sinch.android.rtc.PushPair;
 import com.sinch.android.rtc.Sinch;
 import com.sinch.android.rtc.SinchClient;
 import com.sinch.android.rtc.SinchClientListener;
 import com.sinch.android.rtc.SinchError;
 import com.sinch.android.rtc.calling.Call;
 import com.sinch.android.rtc.calling.CallClient;
+import com.sinch.android.rtc.calling.CallClientListener;
+import com.sinch.android.rtc.calling.CallListener;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -32,46 +37,55 @@ public class VideoAndVoiceChat extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.call);
         init("a");
+        final Toast toast = Toast.makeText(VideoAndVoiceChat.this, "hej", Toast.LENGTH_LONG);
         final Button button = (Button) findViewById(R.id.button);
+
         button.setOnClickListener(new View.OnClickListener() {
 
             //@Override
-            //CallClient callClient = mSinchClient.getCallClient();
+            CallClient callClient = mSinchClient.getCallClient();
             //Call call = callClient.callUser("call-recipient-id");
             //sinchClient.getCallClient().callUser("call-recipient-id");
+
             public void onClick(View view) {
 
                 if (call == null){
-                    call = mSinchClient.getCallClient().callUser("call-recipient-id");
+                    toast.show();
+                    call=mSinchClient.getCallClient().callUser("erik");
+
+
+                    call.addCallListener(new SinchCallListener());
                     button.setText("Hang up");
                 }else {
                     call.hangup();
                     call = null;
                     button.setText("Call");
                 }
-                // make a call!
             }
         });
 
     }
-    protected void init (String username){
-    // Instantiate a SinchClient using the SinchClientBuilder.
+    protected void init (String username) {
+        // Instantiate a SinchClient using the SinchClientBuilder.
 
         android.content.Context context = this.getApplicationContext();
 
-    if (mSinchClient == null) {
-        mSinchClient = Sinch.getSinchClientBuilder().context(context)
-                .applicationKey(APP_KEY)
-                .applicationSecret(APP_SECRET)
-                .environmentHost(ENVIRONMENT)
-                .userId(username)
-                .build();
-        //mSinchClient.checkManifest();
+        if (mSinchClient == null) {
+            mSinchClient = Sinch.getSinchClientBuilder().context(context)
+                    .applicationKey(APP_KEY)
+                    .applicationSecret(APP_SECRET)
+                    .environmentHost(ENVIRONMENT)
+                    .userId(username)
+                    .build();
+        }
+        mSinchClient.setSupportCalling(true);
+        mSinchClient.startListeningOnActiveConnection();
+        mSinchClient.start();
+
+
 
     }
-    mSinchClient.setSupportCalling(true);
-    mSinchClient.startListeningOnActiveConnection();
-
+/*
     mSinchClient.addSinchClientListener(new SinchClientListener() {
         public void onClientStarted(SinchClient client) { }
         public void onClientStopped(SinchClient client) { }
@@ -80,7 +94,6 @@ public class VideoAndVoiceChat extends AppCompatActivity {
         public void onLogMessage(int level, String area, String message) { }
     });
 
-        mSinchClient.start();
 
     //mSinchClient.stopListeningOnActiveConnection();
     //mSinchClient.terminate();
@@ -96,5 +109,29 @@ public class VideoAndVoiceChat extends AppCompatActivity {
         {
             Thread.currentThread().interrupt();
         }
+    }*/
+    private class SinchCallListener implements CallListener{
+        private Button button = (Button) findViewById(R.id.button);
+        @Override
+        public void onCallProgressing(Call call) {
+            button.setText("progressing");
+
+        }
+
+        @Override
+        public void onCallEstablished(Call call) {
+
+        }
+
+        @Override
+        public void onCallEnded(Call call) {
+
+        }
+
+        @Override
+        public void onShouldSendPushNotification(Call call, List<PushPair> list) {
+
+        }
     }
+
 }
