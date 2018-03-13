a22a9d85a6175d10e99b651525b5ea9043753d99
Linnea <linlu607@student.liu.se>
2018-03-13 16:02:25 +0100

    Ny meny

---
+++ Polisappen/app/src/main/java/polis/polisappen/PublicMainActivity.java
@@ -0,0 +1,10 @@
+package polis.polisappen;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by linne on 2018-03-13.
+ */
+
+public class PublicMainActivity extends AppCompatActivity {
+}
